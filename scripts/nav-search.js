869b4dd730e2643d51f6a0d44043cf8d4b31f059
othree <othree@gmail.com>
2012-08-02 23:58:03 +0800

    Add resize to toggle nav search

--- scripts/nav-search.js
+++ scripts/nav-search.js
@@ -1,7 +1,8 @@
 function toggleNavSearch(delay) {
     if (!toggleNavSearch.timerId) {
         toggleNavSearch.timerId = setTimeout(function () {
-            if (document.getElementById('search-form').getBoundingClientRect().top <= 0) {
+            var t = document.getElementById('search-form').getBoundingClientRect().top;
+            if (t <= 0 || t >= 500) {
                 document.getElementById('nav-search').style.visibility = 'visible';
             } else {
                 document.getElementById('nav-search').style.visibility = 'hidden';
@@ -11,5 +12,6 @@ function toggleNavSearch(delay) {
     }
 }
   window.addEventListener('scroll', toggleNavSearch, false);
+  window.addEventListener('resize', toggleNavSearch, false);
   toggleNavSearch(5);
         
