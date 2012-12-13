d54edc1d84795b0da466bbfd8a4f686904cd9623
othree <othree@gmail.com>
2012-12-13 11:16:09 +0800

    Update content

--- scripts/device-pixel-ratio.js
+++ scripts/device-pixel-ratio.js
@@ -1,8 +1,9 @@
 (function () {
+    e = new Date();
+    e.setDate(e.getDate() + 365*10);
+    e = e.toUTCString();
     if (window.devicePixelRatio) {
-        e = new Date();
-        e.setDate(e.getDate() + 365*10);
-        e = e.toUTCString();
         document.cookie = 'devicePixelRatio='+ window.devicePixelRatio +'; expires=' + e;
     }
+    document.cookie = 'w='+ Math.min(window.screen.width, window.screen.height) +'; expires=' + e;
 }());
