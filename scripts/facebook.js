529ba1c31664748cd673b8444b7a96a841d0139d
othree <othree@gmail.com>
2012-12-21 19:27:27 +0800

    Add scripts

---
+++ scripts/facebook.js
@@ -0,0 +1,7 @@
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/zh_TW/all.js#xfbml=1&appId=263583993673371";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
