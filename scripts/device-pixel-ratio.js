5a328820316348c7fb5d779bfaf9455f8b9436f7
othree <othree@gmail.com>
2013-01-05 01:42:38 +0800

    Reset cookie before write

--- scripts/device-pixel-ratio.js
+++ scripts/device-pixel-ratio.js
@@ -3,7 +3,9 @@
     e.setDate(e.getDate() + 365*10);
     e = e.toUTCString();
     if (window.devicePixelRatio) {
+        document.cookie = 'devicePixelRatio=; expires=-1';
         document.cookie = 'devicePixelRatio='+ window.devicePixelRatio +'; expires=' + e;
     }
+    document.cookie = 'w=; expires=-1';
     document.cookie = 'w='+ Math.min(window.screen.width, window.screen.height) +'; expires=' + e;
 }());
