0d3d6672fa27f45c448f64b8f32af24507772ca7
othree <othree@gmail.com>
2023-08-02 12:12:00 +0000

    Commit articles

---
+++ scripts/share.js
@@ -0,0 +1,32 @@
+(function () {
+  "use strict";
+
+  var metas = document.getElementsByTagName('meta'),
+      meta,
+      shareData = {};
+  
+  for (var i = 0; i < metas.length; i += 1) {
+    meta = metas[i];
+
+    if (meta.getAttribute('property') === 'og:title') {
+      shareData.title = meta.getAttribute('content');
+    } else if (meta.getAttribute('property') === 'og:description') {
+      shareData.text = meta.getAttribute('content');
+    } else if (meta.getAttribute('property') === 'og:url') {
+      shareData.url = meta.getAttribute('content');
+    }
+  }
+
+  var shareButton = document.getElementById('share-button');
+
+  if (navigator.share && shareButton) {
+    shareButton.className = 'active';
+    shareButton.addEventListener('click', function () {
+      try {
+        navigator.share(shareData);
+      } catch (e) {}
+    }, false);
+  }
+
+
+}());
